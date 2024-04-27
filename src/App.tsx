ef4176c34b192a03a501c8553d6255f66623b8ad
CCG226 <cown877@gmail.com>
2024-04-27 11:52:36 -0500

    merged code

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import React, { useState } from "react";
+import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import StartModal from "./Components/StartModal";
 import ActionModal from "./Components/ActionModal";
-import React, { useState } from "react";
 
 function App() {
   const [startModalOpen, setStartModalOpen] = useState(true);
@@ -16,9 +17,14 @@ function App() {
   const handleCloseActionModal = () => {
     setActionModalOpen(false);
   };
+  const [dayCounter, setDayCounter] = useState(0);
 
+  const incrementCount = () => {
+    setDayCounter(dayCounter + 1);
+  };
   return (
     <div className="App">
+      <div className="day">Day {dayCounter}</div>
       {/* Render StartModal only if startModalOpen is true */}
       {startModalOpen && (
         <StartModal show={startModalOpen} handleClose={handleCloseStartModal} />
@@ -26,7 +32,10 @@ function App() {
 
       {/* Render ActionModal only if actionModalOpen is true */}
       {actionModalOpen && (
-        <ActionModal show={actionModalOpen} handleClose={handleCloseActionModal} />
+        <ActionModal
+          show={actionModalOpen}
+          handleClose={handleCloseActionModal}
+        />
       )}
     </div>
   );

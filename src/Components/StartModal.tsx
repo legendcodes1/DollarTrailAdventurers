9794032393301df9054169a6ec74eeac362b09c8
CCG226 <cown877@gmail.com>
2024-04-27 09:51:13 -0500

    new bootstrap modal

--- src/Components/StartModal.tsx
+++ src/Components/StartModal.tsx
@@ -1,29 +1,31 @@
-import React, { useEffect } from "react";
-import "../Styles/StartModal.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+
 function StartModal() {
   return (
-    <div className="modalshow startUp">
-      <div className="modal-dialog">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h5 className="modal-title">Modal title</h5>
-          </div>
-          <div className="modal-body">
-            <p>Modal body text goes here.</p>
-          </div>
-          <div className="modal-footer">
-            <button
-              type="button"
-              className="btn btn-secondary"
-              data-bs-dismiss="modal"
-            >
-              Start
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Modal
+      show={true}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Modal heading
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>Centered Modal</h4>
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+          consectetur ac, vestibulum at eros.
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button>Close</Button>
+      </Modal.Footer>
+    </Modal>
   );
 }
 export default StartModal;

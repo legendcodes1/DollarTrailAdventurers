82b427662c9ef05d516bc9e208def17b8235ef6d
legendcodes1 <olafaruq1@gmail.com>
2024-04-27 20:31:45 -0500

    almost final

--- src/Components/ActionModal.tsx
+++ src/Components/ActionModal.tsx
@@ -1,7 +1,9 @@
-import Button from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal";
+import React from "react";
+import { motion } from "framer-motion";
+import { Button, Modal } from "react-bootstrap";
 import "../Styles/modals.css";
 import dollarTrailImage from "../Images/Book.png";
+
 interface ActionModalProps {
   show: boolean;
   handleClose: () => void;
@@ -9,7 +11,25 @@ interface ActionModalProps {
   description: string;
 }
 
-function ActionModal(props: ActionModalProps) {
+const ActionModal: React.FC<ActionModalProps> = (props) => {
+  const modalVariants = {
+    hidden: {
+      opacity: 0,
+      y: -50
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: "spring",
+        stiffness: 120,
+        damping: 12
+      }
+    }
+  };
+
+  const text = props.description.split(" ");
+
   return (
     <Modal
       show={props.show}
@@ -19,20 +39,42 @@ function ActionModal(props: ActionModalProps) {
       centered
       className="myModal"
     >
-      <Modal.Header className="header center">
-        <Modal.Title id="contained-modal-title-vcenter">
-          <img src={require(`../Images/${props.image}`)} />
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body className="mBody center"></Modal.Body>
-      <Modal.Body className="mBody center">
-        <div className="desc"> {props.description}</div>
-      </Modal.Body>
-      <Modal.Footer className="footer center">
-        <Button onClick={props.handleClose}>Yes</Button>
-        <Button onClick={props.handleClose}>No</Button>
-      </Modal.Footer>
+      <motion.div
+        className="modal-content"
+        variants={modalVariants}
+        initial="hidden"
+        animate="visible"
+      >
+        <Modal.Header className="header center">
+          <Modal.Title id="contained-modal-title-vcenter">
+            <img src={require(`../Images/${props.image}`)} />
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="mBody center"></Modal.Body>
+        <Modal.Body className="mBody center">
+          <div className="desc">
+            {text.map((el, i) => (
+              <motion.span
+                key={i}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{
+                  duration: 0.25,
+                  delay: i / 10
+                }}
+              >
+                {el}{" "}
+              </motion.span>
+            ))}
+          </div>
+        </Modal.Body>
+        <Modal.Footer className="footer center">
+          <Button onClick={props.handleClose}>Yes</Button>
+          <Button onClick={props.handleClose}>No</Button>
+        </Modal.Footer>
+      </motion.div>
     </Modal>
   );
-}
+};
+
 export default ActionModal;
